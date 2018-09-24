1a37419a3f7f3b3f11ac8c8b8542f64abe73abd5
pocroianu <pocroianu.nicolae@gmail.com>
2018-09-24 14:46:15 +0300

    chg : PF-3 Test,don't mind it.

--- src/project/abstractClasses/AbstractMediator.ts
+++ src/project/abstractClasses/AbstractMediator.ts
@@ -6,37 +6,72 @@ import * as puremvc from '../../../public/js/puremvc-typescript-multicore-1.1.js
  */
 export class AbstractMediator extends puremvc.Mediator implements puremvc.IMediator {
 
+    /**
+     *
+     * @param mediatorName
+     * @param viewComponent
+     */
     constructor(mediatorName: string, viewComponent: any) {
         super(mediatorName, viewComponent);
     }
 
+    /**
+     *
+     */
     public getMediatorName(): string {
         return super.getMediatorName();
     }
 
+    /**
+     *
+     */
     public getViewComponent(): any {
         return super.getViewComponent();
     }
 
+    /**
+     *
+     * @param viewComponent
+     */
     public setViewComponent(viewComponent: any): void {
         super.setViewComponent(viewComponent);
     }
 
+    /**
+     *
+     */
     public listNotificationInterests(): string[] {
         return super.listNotificationInterests();
     }
 
+    /**
+     *
+     * @param notification
+     */
     public handleNotification(notification: puremvc.INotification): void {
         super.handleNotification(notification);
     }
 
+    /**
+     *
+     */
     public onRegister(): void {
         super.onRegister();
     }
 
+    /**
+     *
+     * @param name
+     * @param body
+     * @param type
+     */
     public sendNotification(name: string, body?: any, type?: string): void {
         super.sendNotification(name, body, type);
     }
+
+    /**
+     *
+     */
     public onRemove(): void{
         super.onRemove();
     }
--- src/project/abstractClasses/AbstractView.ts
+++ src/project/abstractClasses/AbstractView.ts
@@ -9,24 +9,43 @@ export class AbstractView extends puremvc.View {
     private multitonkey: string;
     private _active: boolean;
 
+    /**
+     *
+     * @param key
+     */
     constructor(key: string) {
         super(key);
         this.multitonkey = key;
 
     }
 
+    /**
+     * Returns the view's container
+     */
     public getUIContainer(): PIXI.Container {
         return this.container;
     }
 
+    /**
+     * Initializes the view.
+     * Creates an instance of the container.
+     */
     public initializeView(): void {
         this.container = new PIXI.Container();
     }
 
+    /**
+     * Add a PIXI graphics,sprite,container etc to this view
+     * @param item
+     */
     public addToContainer(item: any): void {
         this.container.addChild(item);
     }
 
+    /**
+     *
+     * @param _active
+     */
     public setActive(_active: boolean): void {
         for (let item of this.container.children) {
             item.visible = _active;
@@ -34,22 +53,40 @@ export class AbstractView extends puremvc.View {
         this._active = _active;
     }
 
+    /**
+     * Register a mediator to this view
+     * @param mediator
+     */
     public registerMediator(mediator: puremvc.Mediator) {
         super.registerMediator(mediator);
     }
 
+    /**
+     *
+     */
     get multitonKey(): string {
         return this.multitonkey;
     }
 
+    /**
+     *
+     * @param key
+     */
     set multitonKey(key: string) {
         this.multitonkey = key;
     }
 
+    /**
+     * Getter for the active class property
+     */
     get active(): boolean {
         return this._active;
     }
 
+    /**
+     * Setter for the active class property
+     * @param key
+     */
     set active(key: boolean) {
         this._active = key;
     }
